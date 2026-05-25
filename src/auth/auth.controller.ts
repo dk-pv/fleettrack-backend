818c653ce66cdf242aac4d375f8b714755d3320f
dk <kukkudanish@gmail.com>
2026-05-25 15:58:02 +0530

    setup basic login backend code and it is working

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
+
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  @Get()
+  test() {
+    return {
+      success: true,
+      message: 'Auth API Working',
+    };
+  }
+
+  @Post('login')
+  login(@Body() body: any) {
+    return this.authService.login(body);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+  ) {}
+
+  async login(body: any) {
+    const { email, password } = body;
+
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+    if (!user) {
+      throw new UnauthorizedException(
+        'Invalid email',
+      );
+    }
+
+    if (user.password !== password) {
+      throw new UnauthorizedException(
+        'Invalid password',
+      );
+    }
+
+    return {
+      success: true,
+      message: 'Login successful',
+      user,
+      token: 'dummy-jwt-token',
+    };
+  }
+}
