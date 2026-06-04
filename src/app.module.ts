52d26625425926ab2793947fd8dd8bedca29f4cf
dk <kukkudanish@gmail.com>
2026-06-04 10:48:07 +0530

    today first

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { AuthModule } from './auth/auth.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { ClientsModule } from './clients/clients.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-
 @Module({
-  imports: [PrismaModule, AuthModule, VehiclesModule, ClientsModule, DashboardModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    VehiclesModule,
+    ClientsModule,
+    DashboardModule,
+  ],
 })
 export class AppModule {}
